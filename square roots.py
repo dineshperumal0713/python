e912835056e28f2a41d81958dd68f3a6c730a545
dineshperumal0713 <cadbury.usha@gmail.com>
2025-09-27 11:08:08 +0100

    square roots added proper

--- square roots.py
+++ square roots.py
@@ -1,3 +1,8 @@
-number = float(input("enter a number to find its sqaure root:"))
-square_root = number**0.5
-print (f"The square root of {number} is {square_root}")
+number = float(input("enter a number to find its sqaure root: "))
+if number < 0:
+    print("Sorry,square root of negative numbers is not defined in real numbers.")
+else:
+    square_root = number**0.5
+    print (f"The square root of {number} is {square_root}")  
+
+
