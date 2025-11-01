fd82b16fa4d2067a65f1fe88b5e7370b4eadbdd0
dineshperumal0713 <cadbury.usha@gmail.com>
2025-11-01 13:47:32 +0000

    done

--- circumfrence.py
+++ circumfrence.py
@@ -1,3 +1,6 @@
-radius=int(input("enter the radius of circle: "))
-sum=radius*4
-print("the circumference of circle is: ",sum)
+def circumfrence():
+    radius = int(input("enter the radius of the circle:  "))
+    circumfrence = 2 * 3.14 * radius
+    print ("the circumfrence of the circle is:", circumfrence)
+     
+circumfrence()s
