0d2ff43c8b91c64889efadf7ed4638720416c9bc
dineshperumal0713 <cadbury.usha@gmail.com>
2025-11-09 11:41:39 +0000

    bye

---
+++ factorials.py
@@ -0,0 +1,8 @@
+def factorial(x):
+   '''This is a recursive function to find the factorial of an integer'''
+   if x==0 or x==1:
+     return 1
+   else:
+     return x*factorial(x-1)
+print(factorial.__doc__)
+print("the factorial of 0 is",factorial(0))
