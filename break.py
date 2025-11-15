13cc62002ae31d12c9ce044f4f9e52387360c6f5
dineshperumal0713 <cadbury.usha@gmail.com>
2025-11-15 11:31:30 +0000

    hello

---
+++ break.py
@@ -0,0 +1,7 @@
+word = str(input("please enter a word :  "))
+for i in word:t
+    if i == "a" or i == "A" :
+        print("this word contains the letter a")
+        break
+    else:
+        print ("there is no a in this word")
