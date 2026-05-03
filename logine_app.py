030e7d70b633917f9246041aabf986683bbdf816
dineshperumal0713 <cadbury.usha@gmail.com>
2026-05-03 12:15:37 +0100

    login app

---
+++ logine_app.py
@@ -0,0 +1,30 @@
+from tkinter import *
+root = Tk()
+root.title("login app")
+root.geometry("400x450")
+frame = Frame(master = root, height = 200, width = 360, bg = "#d0efff")
+lb1 = Label(frame, text = "Full name:", bg = "#3895D3", fg = "white", width = 10)
+lb2 = Label(frame, text = "Email ID:", bg = "#3895D3", fg = "white", width = 10)
+lb3 = Label(frame, text = "Password:", bg = "#3895D3", fg = "white", width = 10)
+name_entry = Entry(frame)
+email_entry = Entry(frame)
+password_entry = Entry(frame)
+def display():
+    name = name_entry.get()
+    greets = "hi " + name
+    message = "\nYou have successfully logged in"
+    textbox.insert(END, greets )
+    textbox.insert(END, message)
+textbox = Text(bg = "#BEBEBE", fg = "black")
+btn = Button(text = "create account", command = display, bg = "teal")
+frame.place(x = 20, y = 0)
+lb1.place(x=20, y=20)
+name_entry.place(x=150 , y=20)
+lb2.place(x=20 , y=80)
+email_entry.place(x=150 , y=80)
+lb3.place(x=20 , y=140)
+password_entry.place(x=150 , y=140)
+btn.place(x=130 , y=210)
+textbox.place(y = 250)
+root.mainloop()
+
